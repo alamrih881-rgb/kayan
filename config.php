318e696149e80e9bd14e2b785f70f7dcc113bcb8
alamrih881-rgb <alamrih881@gmail.com>
2026-04-26 10:43:55 +0400

    Update database connection configuration

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$host = getenv("DB_HOST") ?: "localhost";
-$dbname = getenv("DB_NAME") ?: "kayan_db";
-$username = getenv("DB_USER") ?: "root";
-$password = getenv("DB_PASS") ?: "";
+$host = getenv("MYSQLHOST");
+$dbname = getenv("MYSQLDATABASE");
+$username = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("DB Error: " . $e->getMessage());
